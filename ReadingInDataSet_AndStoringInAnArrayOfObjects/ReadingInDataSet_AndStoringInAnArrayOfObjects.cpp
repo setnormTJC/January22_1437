91de99f98dadaf2982484978e1c7cefefb3cfae4
Work <Work@MSI>
2025-02-05 16:04:21 -0600

    Where we ended on Feb 5.
    can you find bugs?

--- ReadingInDataSet_AndStoringInAnArrayOfObjects/ReadingInDataSet_AndStoringInAnArrayOfObjects.cpp
+++ ReadingInDataSet_AndStoringInAnArrayOfObjects/ReadingInDataSet_AndStoringInAnArrayOfObjects.cpp
@@ -5,6 +5,7 @@
 #include<fstream> 
 
 #include<string> //for `getline` function
+#include <vector>
 
 using std::string; 
 using std::ifstream; 
@@ -19,10 +20,6 @@ struct SocialMediaRecord
     string Gender; 
     string Platform;
     int DailyUsageMinutes;
-    int PostsPerDay;
-    int LikesPerPost;
-    int CommentsPerPost;
-    int SharesPerPost;
 };
 
 struct Person
@@ -31,27 +28,26 @@ struct Person
     double weight; 
 };
 
-int main()
+
+void demoReadingSimpleTextFile()
 {
     //how to read data from an input file?
     //like this: !
-    ifstream fin{"someTextFile.txt"}; //fin is analogous to cin (character input from keyboard) -> file input
+    ifstream fin{ "someTextFile.txt" }; //fin is analogous to cin (character input from keyboard) -> file input
 
 
     //check if the input file was found (before WASTING any time going further in this program)
-    if (! fin.is_open())
+    if (!fin.is_open())
 
     {
         //"early return" 
         cout << " File was NOT found! : (\n";
-        return -1; 
+        return;
     }
 
-    //read in the data of the file: 
-    //string firstWordFromFile; 
 
     //fin >> firstWordFromFile; 
-    string currentLine; 
+    string currentLine; //this variable will hold the contents of the current Line in the file 
 
     //cout << "The first (and ONLY the first) word from that file is: " << firstWordFromFile << "\n";
     while (!fin.eof()) //EOF -> End Of File
@@ -60,11 +56,46 @@ int main()
         cout << currentLine << "\n";
     }
 
-    fin.close(); 
+    fin.close();
+}
 
+int main()
+{
 
+    //demoReadingSimpleTextFile(); 
 
     //demo getline for text and ints (and other data types) 
+
+
+    ifstream fin{ "socialMediaRecords.csv" }; 
+
+    if (!fin)
+    {
+        cout << "FNFE\n";
+        return -1; //early return - waste no time looking through data if the file is not even there!
+    }
+
+
+    std::vector<SocialMediaRecord> recordList; 
+    //let's just print the entire csv file contents in the terminal for now
+    //we'll STORE the data and analyze 
+    while (!fin.eof())
+    {
+        SocialMediaRecord currentRecord; 
+
+        fin >> currentRecord.UserID >> currentRecord.Age >> currentRecord.Gender
+            >> currentRecord.Platform >> currentRecord.DailyUsageMinutes; 
+
+        //string currentLine; 
+
+        //getline(fin, currentLine);
+
+        //cout << currentLine << "\n";
+
+        recordList.push_back(currentRecord);
+        cout << "adsfasdf\n";
+    }
+
 
     return 0; //NORMAL termination (return 0 by convention) 
 }
